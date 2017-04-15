f83a9094edbba22795a3db32f7b9ac980cbe4e54
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-15 02:36:05 +0100

    finished, was realitively easy...i should get better at commiting

--- main/main.go
+++ main/main.go
@@ -43,12 +43,24 @@ func GetPeopleEndPoint(w http.ResponseWriter, req * http.Request){
 
 
 func CreatePersonEndPoint(w http.ResponseWriter, req * http.Request){
-
+	params := mux.Vars(req)
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
 
 func DeletePersonEndPoint(w http.ResponseWriter, req * http.Request){
-
+	params := mux.Vars(req)
+	for index, item := range people{
+		if item.ID == params["id"]{
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 func main(){
